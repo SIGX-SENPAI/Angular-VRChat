4068f052de4e203f8f226feba6c06918fe0e02b5
SIGX-SENPAI <christian2011b@hotmail.com>
2020-08-31 13:44:35 -0500

    making changes on dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AuthGuard } from './guards/auth.guard';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CrudProfileComponent } from './components/shared/crud-profile/crud-profile.component';
 
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'profile', component: CrudProfileComponent, outlet: 'profile'},
   { path: 'community', component: CommunityComponent},
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
